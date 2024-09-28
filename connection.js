51d059689934de9037f06ce7b6dae2fbf1e429aa
anshusaifi <saifianash&@gmail.com>
2024-09-28 12:28:43 +0530

    updation

---
+++ connection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+async function connection(){
+    try {
+
+        await mongoose.connect('mongodb://0.0.0.0:27017/batch-3')
+        console.log("db is connected")
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = connection;
